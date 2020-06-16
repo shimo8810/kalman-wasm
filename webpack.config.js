9d2921016d4a965ae0bc9fbea84e59c147778326
Shimo <shimo.8810@gmail.com>
2020-06-16 21:12:30 +0900

    update: docsからdistへ戻す

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-const dist = path.resolve(__dirname, "docs");
+const dist = path.resolve(__dirname, "dist");
 
 module.exports = {
   mode: "production",
